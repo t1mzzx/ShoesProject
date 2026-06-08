a1b62ff7f6c06486f146f0327e97dd8491e6d27f
t1mzzx <otimon07@gmail.com>
2026-06-08 13:52:21 +0500

    Создание формы FormLogin

---
+++ ShoesProject/FormLogin.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ShoesProject
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            this.pblogo = new System.Windows.Forms.PictureBox();
+            this.pnMain = new System.Windows.Forms.Panel();
+            this.lbLogin = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnGuest = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pblogo)).BeginInit();
+            this.pnMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pblogo
+            // 
+            this.pblogo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pblogo.Image = ((System.Drawing.Image)(resources.GetObject("pblogo.Image")));
+            this.pblogo.Location = new System.Drawing.Point(139, 12);
+            this.pblogo.Name = "pblogo";
+            this.pblogo.Size = new System.Drawing.Size(100, 100);
+            this.pblogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pblogo.TabIndex = 0;
+            this.pblogo.TabStop = false;
+            // 
+            // pnMain
+            // 
+            this.pnMain.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pnMain.Controls.Add(this.btnGuest);
+            this.pnMain.Controls.Add(this.btnLogin);
+            this.pnMain.Controls.Add(this.txtPassword);
+            this.pnMain.Controls.Add(this.lbPassword);
+            this.pnMain.Controls.Add(this.txtLogin);
+            this.pnMain.Controls.Add(this.lbLogin);
+            this.pnMain.Location = new System.Drawing.Point(34, 127);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(320, 222);
+            this.pnMain.TabIndex = 1;
+            // 
+            // lbLogin
+            // 
+            this.lbLogin.AutoSize = true;
+            this.lbLogin.Location = new System.Drawing.Point(141, 17);
+            this.lbLogin.Name = "lbLogin";
+            this.lbLogin.Size = new System.Drawing.Size(52, 19);
+            this.lbLogin.TabIndex = 0;
+            this.lbLogin.Text = "Логин";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(35, 42);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(250, 26);
+            this.txtLogin.TabIndex = 1;
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(138, 74);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(58, 19);
+            this.lbPassword.TabIndex = 2;
+            this.lbPassword.Text = "Пароль";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(35, 99);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(250, 26);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Location = new System.Drawing.Point(92, 130);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(150, 30);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Войти";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // btnGuest
+            // 
+            this.btnGuest.BackColor = System.Drawing.Color.Chartreuse;
+            this.btnGuest.FlatAppearance.BorderSize = 0;
+            this.btnGuest.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuest.Location = new System.Drawing.Point(92, 166);
+            this.btnGuest.Name = "btnGuest";
+            this.btnGuest.Size = new System.Drawing.Size(150, 30);
+            this.btnGuest.TabIndex = 5;
+            this.btnGuest.Text = "Войти как гость";
+            this.btnGuest.UseVisualStyleBackColor = false;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.pnMain);
+            this.Controls.Add(this.pblogo);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Вход в систему";
+            ((System.ComponentModel.ISupportInitialize)(this.pblogo)).EndInit();
+            this.pnMain.ResumeLayout(false);
+            this.pnMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pblogo;
+        private System.Windows.Forms.Panel pnMain;
+        private System.Windows.Forms.Button btnGuest;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Label lbLogin;
+    }
+}
